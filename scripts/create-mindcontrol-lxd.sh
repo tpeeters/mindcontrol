ed2e8d47a5c44e68e4c1319f65008abe6a544d00
Tim Peeters <contact@timpeeters.com>
2017-01-24 17:59:17 +0100

    Call containers containers, not images.

--- scripts/create-mindcontrol-lxd.sh
+++ scripts/create-mindcontrol-lxd.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 if [[ $# -ne 1 ]] ; then
-    echo "Usage:   $0 <image-name>"
+    echo "Usage:   $0 <container-name>"
     echo "Example: $0 mindcontrol"
     exit 0
 fi
 
-IMAGENAME=$1
+CONTAINER=$1
 USER="ubuntu"
 
-function print_image_name {
-    echo "The image name is $1"
+function print_container_name {
+    echo "The container name is $1"
 }
 
 function wait_for_network {
-    echo "Waiting for $IMAGENAME to get network."
+    echo "Waiting for $CONTAINER to get network."
     # TODO. 10 seconds seems enough, but it is better
     #	to really check that the container is up and has network.
     sleep 15
 }
 
 function exec_as_root_in_container {
-    echo "Executing \"$1\" as root in $IMAGENAME."
-    lxc exec $IMAGENAME -- $1
+    echo "Executing \"$1\" as root in $CONTAINER."
+    lxc exec $CONTAINER -- $1
 }
 
 function exec_as_user_in_container {
-    echo "Executing \"$1\" as $USER in $IMAGENAME."
-    lxc exec $IMAGENAME -- su -c "$1" -l $USER
+    echo "Executing \"$1\" as $USER in $CONTAINER."
+    lxc exec $CONTAINER -- su -c "$1" -l $USER
 }
 
-print_image_name $IMAGENAME
-lxc launch ubuntu:x $IMAGENAME
+print_container_name $CONTAINER
+lxc launch ubuntu:x $CONTAINER
 wait_for_network
 exec_as_root_in_container "apt-get -y update"
 exec_as_root_in_container "apt-get -y upgrade"
